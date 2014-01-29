472aa4948b6a91387599b0e816feb50049a19e94
Dave Anderson <anderson@redhat.com>
2014-01-29 15:23:51 -0500

    When invoking a crash session with a compressed vmlinux file, make the same host-machine/vmlinux endian verification that is done with uncompressed vmlinx files. (anderson@redhat.com)

--- symbols.c
+++ symbols.c
@@ -1,8 +1,8 @@
 /* symbols.c - core analysis suite
  *
  * Copyright (C) 1999, 2000, 2001, 2002 Mission Critical Linux, Inc.
- * Copyright (C) 2002-2013 David Anderson
- * Copyright (C) 2002-2013 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2002-2014 David Anderson
+ * Copyright (C) 2002-2014 Red Hat, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -3221,7 +3221,7 @@ is_compressed_kernel(char *file, char **tmp)
 	}
 	please_wait_done();
 
-	if (is_bfd_format(tempname)) {
+	if (is_bfd_format(tempname) && is_kernel(tempname)) {
 		*tmp = tempname;
 		return TRUE;
 	}
