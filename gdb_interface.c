698b34dc2397f304af3cee773e9ccf38c3aa2617
Dave Anderson <anderson@redhat.com>
2014-04-30 15:43:10 -0400

    Fix for the built-in "g" alias, which apparently has not worked correctly since crash-5.1.4.  Without the patch, if the "g" alias and the first argument are separated by one space, then the first first character of that argument would get stripped prior to being passed to the embedded gdb module. (anderson@redhat.com)

--- gdb_interface.c
+++ gdb_interface.c
@@ -1,8 +1,8 @@
 /* gdb_interface.c - core analysis suite
  *
  * Copyright (C) 1999, 2000, 2001, 2002 Mission Critical Linux, Inc.
- * Copyright (C) 2002-2013 David Anderson
- * Copyright (C) 2002-2013 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2002-2014 David Anderson
+ * Copyright (C) 2002-2014 Red Hat, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -788,7 +788,7 @@ cmd_gdb(void)
 	 */
 	if (!is_restricted_command(*argv, FAULT_ON_ERROR)) {
 		if (STREQ(pc->command_line, "gdb")) {
-			strcpy(buf, &pc->orig_line[3]);
+			strcpy(buf, first_space(pc->orig_line));
 			strip_beginning_whitespace(buf);
 		} else
 			strcpy(buf, pc->orig_line);
