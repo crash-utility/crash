cc5244d86b83271700f96995e773ffd2575537b0
Dave Anderson <anderson@redhat.com>
2015-08-14 09:17:30 -0400

    Fix for the S390X "dis" command to prevent jump target addresses from being displayed as kernel system call alias/wrapper names, for example, "SyS_read+<offset>" instead of "sys_read+<offset>". (anderson@redhat.com)

--- s390x.c
+++ s390x.c
@@ -1555,10 +1555,11 @@ s390x_dis_filter(ulong vaddr, char *inbuf, unsigned int output_radix)
 	if ((FIRSTCHAR(argv[argc-1]) == '<') && 
 	    (LASTCHAR(argv[argc-1]) == '>')) {
 		p1 = rindex(inbuf, '<');
-		while ((p1 > inbuf) && !STRNEQ(p1, " 0x")) 
+		while ((p1 > inbuf) && 
+		    !(STRNEQ(p1, " 0x") || STRNEQ(p1, "\t0x") || STRNEQ(p1, ",0x"))) 
 			p1--;
 
-		if (!STRNEQ(p1, " 0x"))
+		if (!(STRNEQ(p1, " 0x") || STRNEQ(p1, "\t0x") || STRNEQ(p1, ",0x"))) 
 			return FALSE;
 		p1++;
 
