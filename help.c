8fef88b2b5bf3ff66807bc952fa19ce03567fd6c
Dave Anderson <anderson@redhat.com>
2017-05-15 12:47:37 -0400

    Update Red Hat copyright to 2017 on initial banner. (anderson@redhat.com)

--- help.c
+++ help.c
@@ -7498,7 +7498,7 @@ display_version(void)
 static 
 char *version_info[] = {
 
-"Copyright (C) 2002-2016  Red Hat, Inc.",
+"Copyright (C) 2002-2017  Red Hat, Inc.",
 "Copyright (C) 2004, 2005, 2006, 2010  IBM Corporation", 
 "Copyright (C) 1999-2006  Hewlett-Packard Co",
 "Copyright (C) 2005, 2006, 2011, 2012  Fujitsu Limited",
