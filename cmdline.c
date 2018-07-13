528849c15a02d9162c2dd17f4551390763711ad5
Dave Anderson <anderson@redhat.com>
2018-07-13 13:50:44 -0400

    Fix for the "repeat" command when the argument consists of an input file construct, for example, "repeat -1 < input_file".  Without the patch, only the first command line in the input file is executed each time. (anderson@redhat.com)

--- cmdline.c
+++ cmdline.c
@@ -1,8 +1,8 @@
 /* cmdline.c - core analysis suite
  *
  * Copyright (C) 1999, 2000, 2001, 2002 Mission Critical Linux, Inc.
- * Copyright (C) 2002-2015,2017 David Anderson
- * Copyright (C) 2002-2015,2017 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2002-2015,2018 David Anderson
+ * Copyright (C) 2002-2015,2018 Red Hat, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -2322,6 +2322,9 @@ is_args_input_file(struct command_table_entry *ct, struct args_input_file *aif)
 	int retval;
 
 	if (pc->curcmd_flags & NO_MODIFY)
+		return FALSE;
+
+	if (STREQ(ct->name, "repeat"))
 		return FALSE;
 
 	BZERO(aif, sizeof(struct args_input_file));
