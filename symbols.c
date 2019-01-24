977c3a20a053eaff5b1f0c95246f88cd66712f27
Dave Anderson <anderson@redhat.com>
2019-01-24 13:36:02 -0500

    Fix for the "p" command if the expression contains more than one opening parenthesis character and a minus/dash sign.  Without the patch, the minus/dash sign will get dropped from the command prior to it being passed on to gdb for evaluation, and the command will fail with the message "p: gdb request failed: <expression>", where the <expression> string will not contain the minus/dash sign. (anderson@redhat.com)

--- symbols.c
+++ symbols.c
@@ -1,8 +1,8 @@
 /* symbols.c - core analysis suite
  *
  * Copyright (C) 1999, 2000, 2001, 2002 Mission Critical Linux, Inc.
- * Copyright (C) 2002-2018 David Anderson
- * Copyright (C) 2002-2018 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2002-2019 David Anderson
+ * Copyright (C) 2002-2019 Red Hat, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -7201,7 +7201,7 @@ cmd_p(void)
 			*(cpuspec-1) = ':';
 	}
 
-	process_gdb_output(concat_args(buf1, 0, TRUE), radix,
+	process_gdb_output(concat_args(buf1, 0, FALSE), radix,
 			   sp ? sp->name : NULL, do_load_module_filter);
 }
 
