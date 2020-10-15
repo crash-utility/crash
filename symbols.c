17e6a44b406cbf07ab8d82cbaf4c52404ec46ba7
Kazuhito Hagio <k-hagio-ab@nec.com>
2020-10-15 11:15:28 +0900

    Fix for failure when using extensions on PPC64 target x86_64 binary
    Without the patch, the "extend" command on an x86_64 binary that can
    be used to analyze ppc64le dumpfiles fails with the error meesage
    "extend: <path to extension>: not an ELF format object".
    
    Suggested-by: Arun Easi <aeasi.linux@gmail.com>
    Signed-off-by: Kazuhito Hagio <k-hagio-ab@nec.com>

--- symbols.c
+++ symbols.c
@@ -3868,7 +3868,8 @@ is_shared_object(char *file)
 			break;
 
 		case EM_X86_64:
-			if (machine_type("X86_64") || machine_type("ARM64")) 
+			if (machine_type("X86_64") || machine_type("ARM64") ||
+			    machine_type("PPC64"))
 				return TRUE;
 			break;
 
