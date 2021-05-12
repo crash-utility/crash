crash-utility/crash

commit 1c45cea02df7f947b4296c1dcaefa1024235ef10

arm64: Change tcr_el1_t1sz variable name to TCR_EL1_T1SZ
Since linux kernel commit bbdbc11804ff ("arm64/crash_core: Export
TCR_EL1.T1SZ in vmcoreinfo") [available in linux-next now], the name
of tcr_el1_t1sz vmcoreinfo variable has been changed to TCR_EL1_T1SZ.

Make a similar change in crash-utility.

Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>

arm64.c
@@ -3922,7 +3922,7 @@ arm64_calc_VA_BITS(void)
 		} else if (ACTIVE())
 			error(FATAL, "cannot determine VA_BITS_ACTUAL: please use /proc/kcore\n");
 		else {
-			if ((string = pc->read_vmcoreinfo("NUMBER(tcr_el1_t1sz)"))) {
+			if ((string = pc->read_vmcoreinfo("NUMBER(TCR_EL1_T1SZ)"))) {
 				/* See ARMv8 ARM for the description of
 				 * TCR_EL1.T1SZ and how it can be used
 				 * to calculate the vabits_actual

netdump.c
@@ -1887,7 +1887,7 @@ vmcoreinfo_read_string(const char *key)
 			sprintf(value, "%ld", nd->arch_data2 & 0xffffffff);
 			return value;
 		}
-		if (STREQ(key, "NUMBER(tcr_el1_t1sz)") && nd->arch_data2) {
+		if (STREQ(key, "NUMBER(TCR_EL1_T1SZ)") && nd->arch_data2) {
 			value = calloc(VADDR_PRLEN+1, sizeof(char));
 			sprintf(value, "%lld", ((ulonglong)nd->arch_data2 >> 32) & 0xffffffff);
 			pc->read_vmcoreinfo = no_vmcoreinfo;

commit 169221f374b1b44e78a4ddd2f356e6ee5870fcaa

Add lowercase tcr_el1_t1sz
Commit 1c45cea "arm64: Change tcr_el1_t1sz variable name to
TCR_EL1_T1SZ", renamed a variable to upper case, but there are
kernels in existence that still have the lower case name, which
breaks crash backwards compatibility.

Fixes: 1c45cea ("arm64: Change tcr_el1_t1sz variable name to
TCR_EL1_T1SZ")

Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>

arm64.c
@@ -3924,7 +3924,8 @@ arm64_calc_VA_BITS(void)
 		} else if (ACTIVE())
 			error(FATAL, "cannot determine VA_BITS_ACTUAL: please use /proc/kcore\n");
 		else {
-			if ((string = pc->read_vmcoreinfo("NUMBER(TCR_EL1_T1SZ)"))) {
+			if ((string = pc->read_vmcoreinfo("NUMBER(TCR_EL1_T1SZ)")) ||
+			    (string = pc->read_vmcoreinfo("NUMBER(tcr_el1_t1sz)"))) {
 				/* See ARMv8 ARM for the description of
 				 * TCR_EL1.T1SZ and how it can be used
 				 * to calculate the vabits_actual

netdump.c
@@ -1927,6 +1927,12 @@ vmcoreinfo_read_string(const char *key)
 			pc->read_vmcoreinfo = no_vmcoreinfo;
 			return value;
 		}
+		if (STREQ(key, "NUMBER(tcr_el1_t1sz)") && nd->arch_data2) {
+			value = calloc(VADDR_PRLEN+1, sizeof(char));
+			sprintf(value, "%lld", ((ulonglong)nd->arch_data2 >> 32) & 0xffffffff);
+			pc->read_vmcoreinfo = no_vmcoreinfo;
+			return value;
+		}
 		if (STREQ(key, "relocate") && nd->arch_data1) {
 			value = calloc(VADDR_PRLEN+1, sizeof(char));
 			sprintf(value, "%lx", nd->arch_data1);