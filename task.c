crash-utility/crash

commit b93027ce5c756764978271c71f6612aeb38f8972

Add alternate HZ calculation using write_expire
Linux 4.8 and later kernels that contain kernel commit 9a7f38c42c2b
("cfq-iosched: Convert from jiffies to nanoseconds") changed the
definition of cfq_slice_async, and it cannot be used to calculate
the HZ value.

Add alternate HZ calculation using write_expire, which depends on
CONFIG_MQ_IOSCHED_DEADLINE or CONFIG_IOSCHED_DEADLINE.

Signed-off-by: Kazuhito Hagio <k-hagio-ab@nec.com>

task.c
@@ -432,6 +432,29 @@ task_init(void)
 			    	    "cfq_slice_async exists: setting hz to %d\n", 
 					machdep->hz);
 		}
+	} else if ((symbol_exists("dd_init_queue") &&
+	    gdb_set_crash_scope(symbol_value("dd_init_queue"), "dd_init_queue")) ||
+	    (symbol_exists("deadline_init_queue") &&
+	    gdb_set_crash_scope(symbol_value("deadline_init_queue"), "deadline_init_queue"))) {
+		char buf[BUFSIZE];
+		uint write_expire = 0;
+
+		open_tmpfile();
+		sprintf(buf, "printf \"%%d\", write_expire");
+		if (gdb_pass_through(buf, pc->tmpfile, GNU_RETURN_ON_ERROR)) {
+			rewind(pc->tmpfile);
+			if (fgets(buf, BUFSIZE, pc->tmpfile))
+				sscanf(buf, "%d", &write_expire);
+		}
+		close_tmpfile();
+
+		if (write_expire) {
+			machdep->hz = write_expire / 5;
+			if (CRASHDEBUG(2))
+				fprintf(fp, "write_expire exists: setting hz to %d\n",
+					machdep->hz);
+		}
+		gdb_set_crash_scope(0, NULL);
 	}
 
 	if (VALID_MEMBER(runqueue_arrays)) 

commit b9dc76e232e0226a14ae3089e3be5c915f2bb981

Fix for HZ calculation on Linux 5.14 and later
Kernel commit 3e9a99eba058 ("block/mq-deadline: Rename dd_init_queue()
and dd_exit_queue()") renamed dd_init_queue to dd_init_sched. Without
the patch, the 'help -m' may print incorrect hz value as follows:

crash> help -m | grep hz
       hz: 1000    <---The correct hz value on ppc64le machine is 100.
	   ^^^^

Fixes: b93027ce5c75 ("Add alternate HZ calculation using write_expire")
Signed-off-by: Lianbo Jiang <lijiang@redhat.com>

task.c
@@ -440,6 +440,8 @@ task_init(void)
 		}
 	} else if ((symbol_exists("dd_init_queue") &&
 	    gdb_set_crash_scope(symbol_value("dd_init_queue"), "dd_init_queue")) ||
+	    (symbol_exists("dd_init_sched") &&
+	    gdb_set_crash_scope(symbol_value("dd_init_sched"), "dd_init_sched")) ||
 	    (symbol_exists("deadline_init_queue") &&
 	    gdb_set_crash_scope(symbol_value("deadline_init_queue"), "deadline_init_queue"))) {
 		char buf[BUFSIZE];