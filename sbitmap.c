df1fd928667a74e8a5f4aef3596d814e95fdff72
Huang Shijie <shijie@os.amperecomputing.com>
2022-03-30 19:03:23 +0000

    diskdump: Optimize the boot time
    1.) The vmcore file maybe very big.
    
        For example, I have a vmcore file which is over 23G,
        and the panic kernel had 767.6G memory,
        its max_sect_len is 4468736.
    
        Current code costs too much time to do the following loop:
        ..............................................
            for (i = 1; i < max_sect_len + 1; i++) {
                    dd->valid_pages[i] = dd->valid_pages[i - 1];
                    for (j = 0; j < BITMAP_SECT_LEN; j++, pfn++)
                            if (page_is_dumpable(pfn))
                                    dd->valid_pages[i]++;
        ..............................................
    
        For my case, it costs about 56 seconds to finish the
        big loop.
    
        This patch moves the hweightXX macros to defs.h,
        and uses hweight64 to optimize the loop.
    
        For my vmcore, the loop only costs about one second now.
    
    2.) Tests result:
      # cat ./commands.txt
          quit
    
     Before:
    
      #echo  3 > /proc/sys/vm/drop_caches;
      #time ./crash -i ./commands.txt /root/t/vmlinux /root/t/vmcore > /dev/null 2>&1
       ............................
            real    1m54.259s
            user    1m12.494s
            sys     0m3.857s
       ............................
    
     After this patch:
    
      #echo  3 > /proc/sys/vm/drop_caches;
      #time ./crash -i ./commands.txt /root/t/vmlinux /root/t/vmcore > /dev/null 2>&1
       ............................
            real    0m55.217s
            user    0m15.114s
            sys     0m3.560s
       ............................
    
    Signed-off-by: Huang Shijie <shijie@os.amperecomputing.com>

--- sbitmap.c
+++ sbitmap.c
@@ -49,25 +49,6 @@ struct sbitmapq_data {
 
 static uint sb_flags = 0;
 
-static inline unsigned int __const_hweight8(unsigned long w)
-{
-	return
-		(!!((w) & (1ULL << 0))) +
-		(!!((w) & (1ULL << 1))) +
-		(!!((w) & (1ULL << 2))) +
-		(!!((w) & (1ULL << 3))) +
-		(!!((w) & (1ULL << 4))) +
-		(!!((w) & (1ULL << 5))) +
-		(!!((w) & (1ULL << 6))) +
-		(!!((w) & (1ULL << 7)));
-}
-
-#define __const_hweight16(w) (__const_hweight8(w)  + __const_hweight8((w)  >> 8))
-#define __const_hweight32(w) (__const_hweight16(w) + __const_hweight16((w) >> 16))
-#define __const_hweight64(w) (__const_hweight32(w) + __const_hweight32((w) >> 32))
-
-#define hweight32(w) __const_hweight32(w)
-#define hweight64(w) __const_hweight64(w)
 
 #define BIT(nr)			(1UL << (nr))
 
