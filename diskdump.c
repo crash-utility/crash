crash-utility/crash

commit 0172e35083b545fa7dd640fa5de0111f8474fc14

Fix "rd" command to display data on zram on Linux 5.17 and later
Fix "rd" command to display data on zram on Linux 5.17 and later kernels
that contain commits
  a41ec880aa7b ("zsmalloc: move huge compressed obj from page to zspage"),
  ffedd09fa9b0 ("zsmalloc: Stop using slab fields in struct page"),
and on Linux 6.1 and later that contain commit
  f635725c3905 ("zram: do not waste zram_table_entry flags bits").

Also, fix a bug that sets the same "byte" by memset() instead to pages
containing the same "unsigned long" elements.

Before:
  crash> mod -s zram zram.ko
       MODULE       NAME                                          BASE           SIZE  OBJECT FILE
  ffffffde224db800  zram                                    ffffffde224d2000    57344  zram.ko
  crash> mod -s zsmalloc zsmalloc.ko
       MODULE       NAME                                          BASE           SIZE  OBJECT FILE
  ffffffde224c5180  zsmalloc                                ffffffde224bf000    40960  zsmalloc.ko
  crash> rd 0x13d89fb0
  rd: zspage magic incorrect: b0

After:
  crash> rd 0x13d89fb0
          13d89fb0:  c2b54f7170883b20                     ;.pqO..

Signed-off-by: chenguanyou <chenguanyou@xiaomi.com>
Signed-off-by: Kazuhito Hagio <k-hagio-ab@nec.com>

diskdump.c
@@ -2755,15 +2755,36 @@ diskdump_device_dump_info(FILE *ofp)
 	}
 }
 
+static ulong ZRAM_FLAG_SHIFT;
+static ulong ZRAM_FLAG_SAME_BIT;
+
 static void
 zram_init(void)
 {
+	long zram_flag_shift;
+
 	MEMBER_OFFSET_INIT(zram_mempoll, "zram", "mem_pool");
 	MEMBER_OFFSET_INIT(zram_compressor, "zram", "compressor");
 	MEMBER_OFFSET_INIT(zram_table_flag, "zram_table_entry", "flags");
 	if (INVALID_MEMBER(zram_table_flag))
 		MEMBER_OFFSET_INIT(zram_table_flag, "zram_table_entry", "value");
 	STRUCT_SIZE_INIT(zram_table_entry, "zram_table_entry");
+	MEMBER_OFFSET_INIT(zspoll_size_class, "zs_pool", "size_class");
+	MEMBER_OFFSET_INIT(size_class_size, "size_class", "size");
+	MEMBER_OFFSET_INIT(zspage_huge, "zspage", "huge");
+
+	if (enumerator_value("ZRAM_LOCK", &zram_flag_shift))
+		;
+	else if (THIS_KERNEL_VERSION >= LINUX(6,1,0))
+		zram_flag_shift = PAGESHIFT() + 1;
+	else
+		zram_flag_shift = 24;
+
+	ZRAM_FLAG_SHIFT = 1 << zram_flag_shift;
+	ZRAM_FLAG_SAME_BIT = 1 << (zram_flag_shift+1);
+
+	if (CRASHDEBUG(1))
+		fprintf(fp, "zram_flag_shift: %ld\n", zram_flag_shift);
 }
 
 static unsigned char *
@@ -2771,9 +2792,11 @@ zram_object_addr(ulong pool, ulong handle, unsigned char *zram_buf)
 {
 	ulong obj, off, class, page, zspage;
 	struct zspage zspage_s;
+	struct zspage_5_17 zspage_5_17_s;
 	physaddr_t paddr;
 	unsigned int obj_idx, class_idx, size;
 	ulong pages[2], sizes[2];
+	ulong zs_magic;
 
 	readmem(handle, KVADDR, &obj, sizeof(void *), "zram entry", FAULT_ON_ERROR);
 	obj >>= OBJ_TAG_BITS;
@@ -2782,11 +2805,20 @@ zram_object_addr(ulong pool, ulong handle, unsigned char *zram_buf)
 
 	readmem(page + OFFSET(page_private), KVADDR, &zspage,
 			sizeof(void *), "page_private", FAULT_ON_ERROR);
-	readmem(zspage, KVADDR, &zspage_s, sizeof(struct zspage), "zspage", FAULT_ON_ERROR);
 
-	class_idx = zspage_s.class;
-	if (zspage_s.magic != ZSPAGE_MAGIC)
-		error(FATAL, "zspage magic incorrect: %x\n", zspage_s.magic);
+	if (VALID_MEMBER(zspage_huge)) {
+		readmem(zspage, KVADDR, &zspage_5_17_s,
+			sizeof(struct zspage_5_17), "zspage_5_17", FAULT_ON_ERROR);
+		class_idx = zspage_5_17_s.class;
+		zs_magic = zspage_5_17_s.magic;
+	} else {
+		readmem(zspage, KVADDR, &zspage_s, sizeof(struct zspage), "zspage", FAULT_ON_ERROR);
+		class_idx = zspage_s.class;
+		zs_magic = zspage_s.magic;
+	}
+
+	if (zs_magic != ZSPAGE_MAGIC)
+		error(FATAL, "zspage magic incorrect: %x\n", zs_magic);
 
 	class = pool + OFFSET(zspoll_size_class);
 	class += (class_idx * sizeof(void *));
@@ -2804,8 +2836,13 @@ zram_object_addr(ulong pool, ulong handle, unsigned char *zram_buf)
 	}
 
 	pages[0] = page;
-	readmem(page + OFFSET(page_freelist), KVADDR, &pages[1],
+	if (VALID_MEMBER(page_freelist))
+		readmem(page + OFFSET(page_freelist), KVADDR, &pages[1],
 			sizeof(void *), "page_freelist", FAULT_ON_ERROR);
+	else
+		readmem(page + OFFSET(page_index), KVADDR, &pages[1],
+			sizeof(void *), "page_index", FAULT_ON_ERROR);
+
 	sizes[0] = PAGESIZE() - off;
 	sizes[1] = size - sizes[0];
 	if (!is_page_ptr(pages[0], &paddr)) {
@@ -2822,9 +2859,13 @@ zram_object_addr(ulong pool, ulong handle, unsigned char *zram_buf)
 	readmem(paddr, PHYSADDR, zram_buf + sizes[0], sizes[1], "zram buffer[1]", FAULT_ON_ERROR);
 
 out:
-	readmem(page, KVADDR, &obj, sizeof(void *), "page flags", FAULT_ON_ERROR);
-	if (!(obj & (1<<10))) { //PG_OwnerPriv1 flag
-		return (zram_buf + ZS_HANDLE_SIZE);
+	if (VALID_MEMBER(zspage_huge)) {
+		if (!zspage_5_17_s.huge)
+			return (zram_buf + ZS_HANDLE_SIZE);
+	} else {
+		readmem(page, KVADDR, &obj, sizeof(void *), "page flags", FAULT_ON_ERROR);
+		if (!(obj & (1<<10))) // PG_OwnerPriv1 flag
+			return (zram_buf + ZS_HANDLE_SIZE);
 	}
 
 	return zram_buf;
@@ -3003,7 +3044,12 @@ try_zram_decompress(ulonglong pte_val, unsigned char *buf, ulong len, ulonglong 
 	readmem(zram_table_entry + OFFSET(zram_table_flag), KVADDR, &flags,
 		sizeof(void *), "zram_table_flag", FAULT_ON_ERROR);
 	if (!entry || (flags & ZRAM_FLAG_SAME_BIT)) {
-		memset(buf, entry, len);
+		ulong *same_buf = (ulong *)GETBUF(PAGESIZE());
+		for (int count = 0; count < PAGESIZE() / sizeof(ulong); count++) {
+			same_buf[count] = entry;
+		}
+		memcpy(buf, same_buf + off, len);
+		FREEBUF(same_buf);
 		goto out;
 	}
 	size = flags & (ZRAM_FLAG_SHIFT -1);

defs.h
@@ -2226,6 +2226,7 @@ struct offset_table {                    /* stash of commonly-used offsets */
 	long module_memory_base;
 	long module_memory_size;
 	long irq_data_irq;
+	long zspage_huge;
 };
 
 struct size_table {         /* stash of commonly-used sizes */
@@ -7210,8 +7211,7 @@ ulong try_zram_decompress(ulonglong pte_val, unsigned char *buf, ulong len, ulon
 #define SECTOR_SHIFT     9
 #define SECTORS_PER_PAGE_SHIFT  (PAGESHIFT() - SECTOR_SHIFT)
 #define SECTORS_PER_PAGE        (1 << SECTORS_PER_PAGE_SHIFT)
-#define ZRAM_FLAG_SHIFT         (1<<24)
-#define ZRAM_FLAG_SAME_BIT      (1<<25)
+
 struct zspage {
     struct {
         unsigned int fullness : 2;
@@ -7221,6 +7221,18 @@ struct zspage {
     };
     unsigned int inuse;
     unsigned int freeobj;
+};
+
+struct zspage_5_17 {
+	struct {
+		unsigned int huge : 1;
+		unsigned int fullness : 2;
+		unsigned int class : 9;
+		unsigned int isolated : 3;
+		unsigned int magic : 8;
+	};
+	unsigned int inuse;
+	unsigned int freeobj;
 };
 
 /*

commit 55a43bcefa20161c7e56ed0e309e90e941f47efc

Fix compilation error and warning with gcc-4.8.5
Fix the following compilation error in diskdump.c and warning in
xen_hyper.c with gcc-4.8.5 (e.g. RHEL7).

- diskdump.c: In function 'try_zram_decompress':
  diskdump.c:3048:3: error: 'for' loop initial declarations are only allowed in C99 mode
     for (int count = 0; count < PAGESIZE() / sizeof(ulong); count++) {
     ^
  diskdump.c:3048:3: note: use option -std=c99 or -std=gnu99 to compile your code
  make[4]: *** [diskdump.o] Error 1
  make[3]: *** [gdb] Error 2
  make[2]: *** [rebuild] Error 2
  make[1]: *** [gdb_merge] Error 2
  make: *** [all] Error 2

- xen_hyper.c: In function 'xen_hyper_x86_pcpu_init':
  xen_hyper.c:387:36: warning: 'init_tss' may be used uninitialized in this function [-Wmaybe-uninitialized]
      xen_hyper_store_pcpu_context_tss(pcc, init_tss, buf);
                                      ^

Fixes: 0172e35083b5 ("Fix "rd" command to display data on zram on Linux 5.17 and later")
Fixes: 9ee564cd1a46 ("xen: get stack address via stack_base array if available")
Signed-off-by: Kazuhito Hagio <k-hagio-ab@nec.com>

diskdump.c
@@ -3044,8 +3044,9 @@ try_zram_decompress(ulonglong pte_val, unsigned char *buf, ulong len, ulonglong 
 	readmem(zram_table_entry + OFFSET(zram_table_flag), KVADDR, &flags,
 		sizeof(void *), "zram_table_flag", FAULT_ON_ERROR);
 	if (!entry || (flags & ZRAM_FLAG_SAME_BIT)) {
+		int count;
 		ulong *same_buf = (ulong *)GETBUF(PAGESIZE());
-		for (int count = 0; count < PAGESIZE() / sizeof(ulong); count++) {
+		for (count = 0; count < PAGESIZE() / sizeof(ulong); count++) {
 			same_buf[count] = entry;
 		}
 		memcpy(buf, same_buf + off, len);

xen_hyper.c
@@ -324,7 +324,7 @@ void
 xen_hyper_x86_pcpu_init(void)
 {
 	ulong cpu_info;
-	ulong init_tss_base, init_tss, stack_base = 0;
+	ulong init_tss_base, init_tss = 0, stack_base = 0;
 	ulong sp;
 	struct xen_hyper_pcpu_context *pcc;
 	char *buf, *bp;