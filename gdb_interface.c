crash-utility/crash

commit 0f39e33d3504f3a17b83574c3be97640460b7eef

arm64: add pac mask to better support gdb stack unwind
Currently, gdb passthroughs of 'bt', 'frame', 'up', 'down',
'info, locals' don't work on arm64 machine enabled pauth.
This is because gdb does not know the lr register actual values
to unwind the stack frames.

Without the patch:
    crash> gdb bt
    #0  __switch_to (prev=0xffffff8001af92c0, next=0xffffff889da7a580) at /proc/self/cwd/common/arch/arm64/kernel/process.c:569
    #1  0x9fc5c5d3602132c0 in ?? ()
    Backtrace stopped: previous frame identical to this frame (corrupt stack?)

With the patch:
    crash> gdb bt
    #0  __switch_to (prev=prev@entry=0xffffff8001af92c0, next=next@entry=0xffffff889da7a580) at /proc/self/cwd/common/arch/arm64/kernel/process.c:569
    #1  0xffffffd3602132c0 in context_switch (rq=0xffffff8a7295a080, prev=0xffffff8001af92c0, next=0xffffff889da7a580, rf=<optimized out>) at /proc/self/cwd/common/kernel/sched/core.c:5515
    #2  __schedule (sched_mode=<optimized out>, sched_mode@entry=2147859424) at /proc/self/cwd/common/kernel/sched/core.c:6843
    #3  0xffffffd3602136d8 in schedule () at /proc/self/cwd/common/kernel/sched/core.c:6917
    ...

Signed-off-by: Guanyou.Chen <chenguanyou@xiaomi.com>

gdb-10.2.patch
@@ -16216,3 +16216,26 @@ exit 0
  	    printf_filtered (_("Backtrace stopped: %s\n"),
  			     frame_stop_reason_string (trailing));
  	}
+--- gdb-10.2/gdb/frame.c.orig
++++ gdb-10.2/gdb/frame.c
+@@ -944,6 +944,10 @@ frame_find_by_id (struct frame_id id)
+   return NULL;
+ }
+
++#ifdef CRASH_MERGE
++extern "C" void crash_decode_ptrauth_pc(ulong* pc);
++#endif
++
+ static CORE_ADDR
+ frame_unwind_pc (struct frame_info *this_frame)
+ {
+@@ -974,6 +978,9 @@ frame_unwind_pc (struct frame_info *this_frame)
+       try
+	{
+	  pc = gdbarch_unwind_pc (prev_gdbarch, this_frame);
++#ifdef CRASH_MERGE
++	  crash_decode_ptrauth_pc(&pc);
++#endif
+	  pc_p = true;
+	}
+       catch (const gdb_exception_error &ex)

gdb_interface.c
@@ -1083,3 +1083,13 @@ int crash_get_current_task_reg (int regno, const char *regname,
 	return machdep->get_current_task_reg(regno, regname, regsize, value);
 }
 
+/* arm64 kernel lr maybe has patuh */
+void crash_decode_ptrauth_pc(ulong *pc);
+void crash_decode_ptrauth_pc(ulong *pc)
+{
+#ifdef ARM64
+	struct machine_specific *ms = machdep->machspec;
+	if (is_kernel_text(*pc | ms->CONFIG_ARM64_KERNELPACMASK))
+		*pc |= ms->CONFIG_ARM64_KERNELPACMASK;
+#endif /* !ARM64 */
+}

commit 2724bb1d0260f3886f8a3d533838caf80c7e61e5

Fix build failure on 32bit machine(i686)
This issue was caused by commit 0f39e33d3504 with the following
compilation error:

  frame.c: In function ‘CORE_ADDR frame_unwind_pc(frame_info*)’:
  frame.c:982:35: error: cannot convert ‘CORE_ADDR*’ {aka ‘long long unsigned int*’} to ‘ulong*’ {aka ‘long unsigned int*’}
    982 |           crash_decode_ptrauth_pc(&pc);
        |                                   ^~~
        |                                   |
        |                                   CORE_ADDR* {aka long long unsigned int*}
  frame.c:948:48: note: initializing argument 1 of ‘void crash_decode_ptrauth_pc(ulong*)’
    948 | extern "C" void crash_decode_ptrauth_pc(ulong* pc);
        |                                         ~~~~~~~^~

Fixes: 0f39e33d3504 ("arm64: add pac mask to better support gdb stack unwind")
Reported-by: Guanyou.Chen <chenguanyou@xiaomi.com>
Signed-off-by: Lianbo Jiang <lijiang@redhat.com>

gdb-10.2.patch
@@ -55,7 +55,7 @@ exit 0
  # your system doesn't have fcntl.h in /usr/include (which is where it
  # should be according to Posix).
 -DEFS = @DEFS@
-+DEFS = -DCRASH_MERGE @DEFS@
++DEFS = -DCRASH_MERGE -D${CRASH_TARGET} @DEFS@
  GDB_CFLAGS = -I. -I$(srcdir) -I$(srcdir)/config \
  	-DLOCALEDIR="\"$(localedir)\"" $(DEFS)
  
@@ -16222,7 +16222,7 @@ exit 0
    return NULL;
  }
 
-+#ifdef CRASH_MERGE
++#if defined(CRASH_MERGE) && defined(ARM64)
 +extern "C" void crash_decode_ptrauth_pc(ulong* pc);
 +#endif
 +
@@ -16233,7 +16233,7 @@ exit 0
        try
 	{
 	  pc = gdbarch_unwind_pc (prev_gdbarch, this_frame);
-+#ifdef CRASH_MERGE
++#if defined(CRASH_MERGE) && defined(ARM64)
 +	  crash_decode_ptrauth_pc(&pc);
 +#endif
 	  pc_p = true;

gdb_interface.c
@@ -1084,12 +1084,12 @@ int crash_get_current_task_reg (int regno, const char *regname,
 }
 
 /* arm64 kernel lr maybe has patuh */
+#ifdef ARM64
 void crash_decode_ptrauth_pc(ulong *pc);
 void crash_decode_ptrauth_pc(ulong *pc)
 {
-#ifdef ARM64
 	struct machine_specific *ms = machdep->machspec;
 	if (is_kernel_text(*pc | ms->CONFIG_ARM64_KERNELPACMASK))
 		*pc |= ms->CONFIG_ARM64_KERNELPACMASK;
-#endif /* !ARM64 */
 }
+#endif /* !ARM64 */