crash-utility/crash

commit aa9f7248075c701e565b59ec2ebf80c8f76b30f6

Fix for "help -r" segfault in case of ramdump
When the ELF note does not contain CPU registers, attempting to retrieve
online CPU registers will cause a crash. This is likely to happen for
ramdump cases as follows, where the ELF header is created by crash at
runtime.

  crash> help -r
  ...
   WARNING: cpu 7: cannot find NT_PRSTATUS note
         KERNEL: vmlinux  [TAINTED]
      DUMPFILES: /var/tmp/ramdump_elf_RabECD [temporary ELF header]
                 DDRCS0_0.BIN
                 ...
                 DDRCS2_2.BIN
           CPUS: 8
  ...

With the patch:
  crash> help -r
  ...
  CPU 6:
  help: registers not collected for cpu 6
  ...

Lets add a sanity check to avoid the current issue.

Signed-off-by: Guanyou.Chen <chenguanyou@xiaomi.com>

netdump.c
@@ -2768,7 +2768,8 @@ display_regs_from_elf_notes(int cpu, FILE *ofp)
 		}
 	}
 
-	if ((cpu - skipped_count) >= nd->num_prstatus_notes &&
+	if (((cpu < 0 ) || (!nd->nt_prstatus_percpu[cpu]) ||
+		(cpu - skipped_count) >= nd->num_prstatus_notes) &&
 	     !machine_type("MIPS")) {
 		error(INFO, "registers not collected for cpu %d\n", cpu);
 		return;

commit 42fbae46441c54d959227ea4047778872cab2974

Fix a regression of "help -r" which fail to print regs
This patch fixed a regression introduced by commit aa9f724 ("Fix for "help -r"
segfault in case of ramdump"), which ignored the case which nd->nt_prstatus
may contain register notes. As a result, it fails to print the registers
of such case.

Before:
    crash> help -r
    CPU 0:
    help: registers not collected for cpu 0

    CPU 1: [OFFLINE]

After:
    crash> help -r
    CPU 0:
	RIP: ffffffff800c4d92  RSP: ffff810066cc7d68  RFLAGS: 00010246
	RAX: 0000000000000000  RBX: 0000000000000000  RCX: 0000000000000000
	RDX: 650001bc7e664240  RSI: 0000000000000000  RDI: 0000000000000000
	RBP: 650001bc7e664240   R8: 0000000000000006   R9: ffff810069dd03d4
	R10: ffff81007d942080  R11: ffffffff80154235  R12: 0000000000000000
	R13: 0000000000000000  R14: 000000000001e5fe  R15: 0000000000000001
	CS: 0010  SS: 0018

    CPU 1: [OFFLINE]

Signed-off-by: Tao Liu <ltao@redhat.com>

netdump.c
@@ -2768,9 +2768,10 @@ display_regs_from_elf_notes(int cpu, FILE *ofp)
 		}
 	}
 
-	if (((cpu < 0 ) || (!nd->nt_prstatus_percpu[cpu]) ||
-		(cpu - skipped_count) >= nd->num_prstatus_notes) &&
-	     !machine_type("MIPS")) {
+	if (((cpu < 0 ) ||
+	     ((!nd->nt_prstatus_percpu[cpu]) && (!nd->nt_prstatus)) ||
+	     (cpu - skipped_count) >= nd->num_prstatus_notes)
+	    && !machine_type("MIPS")) {
 		error(INFO, "registers not collected for cpu %d\n", cpu);
 		return;
 	}