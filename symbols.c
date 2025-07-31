d2fb3f62861355154b98a09cabe330dc3c4e5443
Pnina Feder <pnina.feder@mobileye.com>
2025-07-31 14:36:15 +0300

    Support running on X86_64 with RISCV target
    Signed-off-by: Pnina Feder <pnina.feder@mobileye.com>

--- symbols.c
+++ symbols.c
@@ -4539,7 +4539,7 @@ is_shared_object(char *file)
 
 		case EM_X86_64:
 			if (machine_type("X86_64") || machine_type("ARM64") ||
-			    machine_type("PPC64"))
+			    machine_type("PPC64") || machine_type("RISCV64"))
 				return TRUE;
 			break;
 
